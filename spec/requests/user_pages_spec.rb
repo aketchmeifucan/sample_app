47e95b1f498593ae5776b309a35cc9187ef57a65
Andrew Ketchum <andrew_ketchum@taylor.edu>
2012-10-23 17:40:38 -0400

    CH8 Ex2

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -7,15 +7,15 @@ describe "User pages" do
 	describe "signup page" do
 		before { visit signup_path }
 
-		it { should have_selector('h1',    text: 'Sign up') }
-		it { should have_selector('title', text: full_title('Sign up')) }
+		it {should have_h1('Sign up')}
+		it {should have_title(full_title('Sign up'))}
 	end
 
 	describe "profile page" do
 		let(:user) {FactoryGirl.create(:user) }
 		before {visit user_path(user) }
-		it{should have_selector('h1', text: user.name) }
-		it{should have_selector('title', text: user.name)}
+		it{should have_h1(user.name)}
+		it{should have_title(user.name)}
 	end
 
 	describe "signup" do
@@ -30,24 +30,19 @@ describe "User pages" do
 
 			describe "after submission" do
 				before {click_button submit }
-				it {should have_selector('title', text: 'Sign up')}
+				it {should have_title('Sign up')}
 				it {should have_content('error')}
 			end
 		end
 
 		describe "with valid information" do
-			before do
-				fill_in "Name", with: "Example User"
-				fill_in "Email", with: "user@example.com"
-				fill_in "Password", with: "foobar"
-				fill_in "Confirmation", with: "foobar"
-			end
+			before {valid_signup}
 			
 			describe "after saving the user" do
 				before {click_button submit }
 				let(:user) {User.find_by_email('user@example.com')}
-				it {should have_selector('title',text: user.name)}
-				it {should have_selector('div.alert.alert-success', text: 'Welcome')}
+				it {should have_title(user.name)}
+				it {should have_success_message('Welcome')}
 				it {should have_link('Sign out') }
 			end
 
