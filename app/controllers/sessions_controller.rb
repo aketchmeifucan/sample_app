19b65cb14e96160c3b9a629645d6f6b143c7b11e
Andrew Ketchum <andrew_ketchum@taylor.edu>
2012-10-30 23:52:02 -0400

    Finish user edit, update, index, and destroy actions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,8 +7,10 @@ class SessionsController < ApplicationController
 #		user = User.find_by_email(params[:session][:email].downcase)
 		user = User.find_by_email(params[:email])
 		if user && user.authenticate(params[:password])
+#		if user && user.authenticate(params[:session][:password])
 			sign_in user
-			redirect_to user
+			redirect_back_or user
+#			redirect_to user
 		else
 			flash.now[:error] = 'Invalid email/password combination'
 			render 'new'
--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -13,6 +13,15 @@ def valid_signup
 	fill_in "Confirmation", with: "foobar"
 end
 
+def sign_in(user)
+	visit signin_path
+	fill_in "Email",    with: user.email
+	fill_in "Password", with: user.password
+	click_button "Sign in"
+	# Sign in when not using Capybara as well.
+	cookies[:remember_token] = user.remember_token
+end
+
 RSpec::Matchers.define :have_error_message do |message|
 	match do |page|
 		page.should have_selector('div.alert.alert-error', text: message)
@@ -45,3 +54,4 @@ end
 #		"#{base_title} | #{page_title}"
 #	end
 #end
+
